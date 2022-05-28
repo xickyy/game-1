4bf1db3971d9f965c481d4ba2da68a146ff066a0
ricky <ricky_moore@ymail.com>
2022-05-27 21:59:39 -0700

    added question 2

--- game-1.js
+++ game-1.js
@@ -7,16 +7,28 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-console.log('Welcome! to how well do you know ricky!?!?');
+console.log('Welcome! To how well do you know Ricky!?!?');
 console.log('******************************************')
 console.log('Question 1-->')
 
-
-const run = function() {
-    rl.question('How old is ricky? ', (answer) => {
-        if (answer === '26') {
+const run2 = function () {
+    console.log('Question 2-->')
+    rl.question('True or False - Ricky loves to go snowboarding? ', (answer2) => {
+        if (answer2.toLowerCase() === 'true') {
             console.log('CORRECT!')
             rl.close();
+        } else {
+            console.log('Nope! try again');
+            run2();
+        }
+    });
+}
+
+const run = function () {
+    rl.question('How old is Ricky? ', (answer) => {
+        if (answer === '26') {
+            console.log('CORRECT!')
+            run2();
         } else {
             console.log('Nope! try again!');
             run();
