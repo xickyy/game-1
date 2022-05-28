c7a37d3004be4289692bf252e023b673f1a8d164
ricky <ricky_moore@ymail.com>
2022-05-27 23:48:20 -0700

    added question 3

--- game-1.js
+++ game-1.js
@@ -7,22 +7,35 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-console.log('Welcome! To how well do you know Ricky!?!?');
-console.log('******************************************')
-console.log('Question 1-->')
+console.log('Welcome to how well do you know Ricky!?!?');
+console.log('*****************************************');
+console.log('Question 1-->');
+
+const run3 = function () {
+    console.log('Question 3-->');
+    rl.question('Does Ricky prefer Windows or Mac? ', (answer3) => {
+        if (answer3.toLowerCase() === 'windows') {
+            console.log('Congratulations! you got all the questions right!' );
+            rl.close();
+        } else {
+            console.log('Nope! try again');
+            run3();
+        }
+    });
+};
 
 const run2 = function () {
-    console.log('Question 2-->')
+    console.log('Question 2-->');
     rl.question('True or False - Ricky loves to go snowboarding? ', (answer2) => {
         if (answer2.toLowerCase() === 'true') {
             console.log('CORRECT!')
-            rl.close();
+            run3();;
         } else {
             console.log('Nope! try again');
             run2();
         }
     });
-}
+};
 
 const run = function () {
     rl.question('How old is Ricky? ', (answer) => {
